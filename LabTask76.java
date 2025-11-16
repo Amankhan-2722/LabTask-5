ec75566511e83e786cff69f7bbffd421fa7f58c5
Amankhan-2722 <amancui2722@gmail.com>
2025-11-16 17:27:04 +0500

    Task 6

---
+++ LabTask76.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class LabTask76{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input: ");
+        String s = input.nextLine();
+
+        int spaceIndex = s.indexOf(" ");
+        String firstWord = s.substring(0, spaceIndex);
+        String secondWord = s.substring(spaceIndex + 1);
+
+        System.out.println("Correct Answer: " + secondWord + " " + firstWord);
+    }
+}
